d37441b3c5b56328a16f40f79651dcd9e9761390
thenml <n.beznosoff@gmail.com>
2022-11-14 03:38:43 +0400

    added size controll

--- en/coolie_recipes.js
+++ en/coolie_recipes.js
@@ -1,4 +1,6 @@
 let tooltips = document.getElementsByClassName('mc-tooltip');
+let mcguis = document.getElementsByClassName('mc-gui');
+
 const onMouseMove = (e) =>{
     for(const item of tooltips) {
         if (item.parentElement.matches(':hover')){
@@ -10,4 +12,16 @@ const onMouseMove = (e) =>{
         }
     }
 }
+const onScroll = () =>{
+    for(const item of tooltips) {
+        item.style.display = 'none';
+    }
+}
+function resizeItems(button){
+    for(const item of mcguis) {
+        item.classList.toggle('mc-gui-size-s')
+    }    
+    button.firstElementChild.classList.toggle('item-img-next')
+}
 document.addEventListener('mousemove', onMouseMove);
+document.addEventListener('scroll', onScroll);
