96fbaa0a3c501ed955a6cefe6ef9d1230a39e699
thenml <n.beznosoff@gmail.com>
2023-01-18 23:33:05 +0400

    dima debt
    (this is a meme page)

---
+++ p/dima_debt/script.js
@@ -0,0 +1,25 @@
+function update(rn){
+    var hours = Math.floor(Math.abs(rn.getTime() - 1674054000000) / 36e5);
+    var days = Math.floor(Math.abs(rn.getTime() - 1673035200000) / 864e5);
+
+    debt = 400;
+    for (let i = 0; i < hours; i++) {
+        debt *= 1.001;
+    }
+    for (let i = 0; i < days; i++) {
+        debt *= 1.01;
+    }
+    debt = Math.round(debt*100) / 100
+
+    $('.ammount').append(
+        `<h2>${debt} руб</h2>`
+    )
+}
+
+update(new Date());
+setInterval(function(){
+    rn = new Date();
+    if(rn.getMinutes() === 0) {
+        update(rn);
+    }
+},60000)
