1f9c615ed1a3bb0291f0266bbee03b9a8ecf6ad5
thenml <n.beznosoff@gmail.com>
2023-01-26 13:53:13 +0400

    add date picker

--- p/dima_debt/script.js
+++ p/dima_debt/script.js
@@ -16,12 +16,29 @@ function update(rn){
     )
 }
 
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+
+
 update(new Date());
+
 setInterval(function(){
     rn = new Date();
     if(rn.getMinutes() === 0) {
         update(rn);
+        $('#date').val(rn.toDateInputValue());
     }
 },60000)
+
+
+$('#date').val(new Date().toDateInputValue());
+$('#date').on('input', function(){
+    update(new Date($('#date').val()));
+});
+
 
 var translations = {}
