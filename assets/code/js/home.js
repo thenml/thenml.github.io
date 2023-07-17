1eedaa1d3114c12ffca72ffb765562672ae7874c
nml <h>
2023-07-17 14:40:13 +0400

    update home page

--- assets/code/js/home.js
+++ assets/code/js/home.js
@@ -1,6 +1,6 @@
 function rotateCard(event, element) {
 	const x = event.pageX - element.offsetLeft;
-    const y = event.pageY - element.offsetTop;
+	const y = event.pageY - element.offsetTop;
 	const middleX = $(element).width() / 2;
 	const middleY = $(element).height() / 2;
 
@@ -19,10 +19,10 @@ function resetCard(event, element) {
 }
 
 $('#cards').children().each((index, card) => {
-	$(card).mousemove( (e) => {
+	$(card).mousemove((e) => {
 		rotateCard(e, card);
 	});
-	$(card).mouseleave( (e) => {
+	$(card).mouseleave((e) => {
 		resetCard(e, card);
 	});
 })
@@ -30,58 +30,29 @@ $('#cards').children().each((index, card) => {
 
 
 //#region background cubes
-background = $('#background');
-
 cubeAmmount = 40;
 for (let i = 0; i < cubeAmmount; i++) {
 	const width = (Math.random() * 50 + 25).toFixed();
-	const posx = (Math.random() * 12 - 2).toFixed();
-	const posy = (i * cubeAmmount / 4).toFixed();
-	const direction = i % 2 ? 'left' : 'right';
+	const posx = (Math.random() - 0.25).toFixed(4);
+	const direction = i % 2 ? '--left' : '--right';
 	const rotation = (Math.random() * 360).toFixed();
 
 	cube = $("<li></li>").css({
 		'width': width + 'px',
-		'height': width + 'px',
-		'bottom': posy + '%',
 		'rotate': rotation + 'deg',
+		'--mobile': (i/cubeAmmount).toFixed(4)
 	});
-	cube.css(direction, posx + '%');
-	cube.appendTo(background);
+	cube.css(direction, posx);
+
+	const duration = Math.random() * 5 + 20;
+	const rotate = Math.random() * 4 + 8;
+	const start = -duration * Math.random();
+	cube.css('animation', `
+		cubes ${duration}s linear infinite ${start}s,
+		cubes-init 1s ease-out,
+		cubes-rotate ${rotate}s linear infinite 1s
+	`);
+
+	cube.appendTo($('#background'));
 }
-
-let previousTimeStamp;
-function cubeAnimation(timestamp) {
-	if (previousTimeStamp === undefined) {
-		previousTimeStamp = timestamp;
-	}
-	const timeDelta = timestamp - previousTimeStamp;
-
-	if (previousTimeStamp !== timestamp) {
-		const move = 0.006 * timeDelta;
-		const globalHeight = $(background).height();
-
-		background.children().each((index, cube) => {
-			const speed = parseInt($(cube).css('width')) / 50;
-			const progress = parseFloat($(cube).css('bottom')) / globalHeight;
-
-			const pos = (progress * 100 + move * speed) % 110;
-			$(cube).css({
-				'opacity': progress,
-				'border-radius': progress * 50 + '%',
-				'bottom': pos + '%'
-			});
-		});
-	}
-
-	previousTimeStamp = timestamp;
-	window.requestAnimationFrame(cubeAnimation);
-}
-
-window.requestAnimationFrame(cubeAnimation);
-setTimeout(function () {
-	background.children().each((index, cube) => {
-		$(cube).css('animation', 'cubes 8s linear infinite')
-	});
-}, 1000);
 //#endregion
